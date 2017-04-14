48e8636ebb4c788a835ff435c00efdf2177e33a9
ikirsten <i.kirsten@auik.de>
2017-04-14 02:23:05 +0200

    Testweise Zeitsteuerung

--- tuersteuerung.py
+++ tuersteuerung.py
@@ -1,1 +1,38 @@
+#---import---
+import config #config file
+import time #time module
 
+
+
+def timecheck():
+
+	#Werktag pruefen	
+	if time.strftime("%w") == '0': #Sonntag
+		check = 0
+		return (bool(check))
+	
+	elif time.strftime("%w") > '0' and time.strftime("%w") < '6': #Werktags
+		start	= config.accesstimes['startweekdays']
+		end	= config.accesstimes['endweekdays']
+
+	
+	elif time.strftime("%w") == '6': #Samstag 
+		start	= config.accesstimes['startsaturday']
+		end	= config.accesstimes['endsaturday']
+	
+	#Pruefen der Uhrzeit	
+	if  time.strftime("%H:%M") < end and start < time.strftime("%H:%M"):
+		if time2min(end) - time2min(time.strftime("%H:%M")) > time2min(config.accesstimes['endbefore']):
+			check = 1	#Ausgabe, wenn im Zeitraum
+		else:
+			check = 0	#Ausgabe, wenn zu knapp vor Ende
+	else:
+		check = 0	#Ausgabe, wenn nicht in Zeitraum
+
+	#Rueckgabe des Ergebniss	
+	return bool(check)
+
+def time2min(x):
+   return reduce(lambda a, b: 60 * int(a) + int(b), x.split(':'))
+
+print(timecheck())
