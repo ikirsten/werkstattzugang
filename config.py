6b07d395fc85dc8ff8d7a4fc222628235a0e3c71
ikirsten <i.kirsten@auik.de>
2017-04-14 03:23:50 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+
+
+#---Access times---
+
+accesstimes = dict(
+	startweekdays	= '07:00', #Starttime on weekdays
+	endweekdays	= '21:30', #Endtime on weekdays
+	startsaturday	= '08:00', #Starttime on saturdays
+	endsaturday	= '18:00', #Endtime on saturdays
+	endbefore	= '00:10', #Letzerzugang vor Ende Eintrittszeit
+)
+
+
+
+
+
+
